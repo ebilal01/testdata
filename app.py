f96bacebc22edac3c6c3161eba15a7d7f5d614cb
ebilal01 <127566393+ebilal01@users.noreply.github.com>
2025-01-29 19:09:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import struct
 import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route('/')
 def index():
